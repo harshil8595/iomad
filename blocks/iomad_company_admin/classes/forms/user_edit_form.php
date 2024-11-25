94d291d208052222126205aef5d52f511b201e17
Derick Turner <derick@e-learndesign.co.uk>
2024-11-25 15:47:32 +0000

    IOMAD: validation of username on user creations should be client side - closes  #1538

--- blocks/iomad_company_admin/classes/forms/user_edit_form.php
+++ blocks/iomad_company_admin/classes/forms/user_edit_form.php
@@ -27,6 +27,8 @@ defined('MOODLE_INTERNAL') || die;
 
 use \company;
 use \iomad;
+use core_user;
+use core_text;
 
 //class user_edit_form extends company_moodleform {
 class user_edit_form extends \moodleform {
@@ -332,6 +334,17 @@ class user_edit_form extends \moodleform {
 
         $usernew = (object)$usernew;
 
+        // Check allowed characters.
+        if (!$usernew->use_email_as_username) {
+            if (empty($usernew->username)) {
+                $errors['username'] = get_string('required');
+            } else if ($usernew->username !== core_text::strtolower($usernew->username)) {
+                $errors['username'] = get_string('usernamelowercase');
+            } else if ($usernew->username !== core_user::clean_field($usernew->username, 'username')) {
+                    $errors['username'] = get_string('invalidusername');
+            }
+        }
+
         // Validate email.
         if (empty($CFG->allowaccountssameemail) &&
             $DB->record_exists('user', array('email' => $usernew->email, 'mnethostid' => $CFG->mnet_localhost_id))) {
@@ -377,6 +390,7 @@ class user_edit_form extends \moodleform {
                 }
             }
         }
+
         return $errors;
     }
 
