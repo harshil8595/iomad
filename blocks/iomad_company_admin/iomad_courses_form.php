464db2999c1b889b784914f227190e6143bf1a75
Derick Turner <derick@e-learndesign.co.uk>
2024-12-01 15:40:12 +0000

    IOMAD: Manager viewing IOMAD courses has too much power. - closes #2081

--- blocks/iomad_company_admin/iomad_courses_form.php
+++ blocks/iomad_company_admin/iomad_courses_form.php
@@ -372,7 +372,7 @@ if (iomad::has_capability('block/iomad_company_admin:company_add', $systemcontex
     $tablecolumns[] = 'shared';	
 }
 // If not editing, show course visibility. Otherwise use the actions column
-if (empty($USER->editing) || !empty($USER->editing) && !has_capability('moodle/course:visibility',  $systemcontext)){
+if (empty($USER->editing)){
     $tableheaders[] = get_string('coursevisibility');
     $tablecolumns[] = 'coursevisibility';
 }
